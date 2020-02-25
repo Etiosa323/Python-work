ad203cd888863e65ae8419f85a4536455584a255
nsahsspecial1 <nsahsspecial1@usi.uncommonschools.org>
2020-02-25 15:19:50 -0500

    assignment one

---
+++ variable.py
@@ -0,0 +1,18 @@
+name="Etiosa"
+age=15
+print("my name is", name, "and i am",age, "years old")
+red=1.0
+future= red+age
+print("next year i will be", future, "years old")
+randomInt=5
+randomFloat=1.0
+randomBool= True
+randomNone = None
+randomString="sup"
+print(type(randomInt))
+print(type(randomFloat))
+print(type(randomBool))
+print(type(randomNone))
+print(type(randomString))
+
+print("my name is" +name +"and i am" + str(age))
